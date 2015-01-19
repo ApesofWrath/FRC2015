c3c070c36335551c54292b1f45ef9d3b28534a9c
Gautam Prabhu <gautamprab@gmail.com>
2015-01-18 16:27:53 -0800

    initial commit

---
+++ src/org/usfirst/frc/team668/robot/Robot.java
@@ -0,0 +1,180 @@
+
+package org.usfirst.frc.team668.robot;
+
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.vision.AxisCamera;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+	public static OI operatorInterface;
+	public static Joystick joystickLeft, joystickRight, joystickOp;
+	public static CANTalon canTalonFrontLeft, canTalonFrontRight, canTalonRearLeft,
+	canTalonRearRight, canTalonIntakeLeft, canTalonIntakeRight,
+	canTalonElevator;
+	public static Encoder encoderLeft;
+	public static Encoder encoderRight;
+	public static Servo camServoVert, camServoHor;
+	public static AxisCamera camera;
+	public static Compressor compressor1;
+	public static DoubleSolenoid leftHugPiston, rightHugPiston, intakePiston;
+	public static RobotDrive robotDrive;
+	
+	boolean isTankDrive = true;
+	
+	
+    public void robotInit() {
+    	operatorInterface = new OI();
+    	joystickLeft = new Joystick(RobotMap.JOYSTICK_LEFT_PORT);
+		joystickRight = new Joystick(RobotMap.JOYSTICK_RIGHT_PORT);
+		joystickOp = new Joystick(RobotMap.JOYSTICK_OP_PORT);
+
+		canTalonFrontLeft = new CANTalon(RobotMap.DRIVE_MOTOR_FRONT_LEFT_CANID);
+		canTalonFrontRight = new CANTalon(RobotMap.DRIVE_MOTOR_FRONT_RIGHT_CANID);
+		canTalonRearLeft = new CANTalon(RobotMap.DRIVE_MOTOR_REAR_LEFT_CANID);
+		canTalonRearRight = new CANTalon(RobotMap.DRIVE_MOTOR_REAR_RIGHT_CANID);
+
+		canTalonIntakeLeft = new CANTalon(RobotMap.INTAKE_MOTOR_LEFT_CANID);
+		canTalonIntakeRight = new CANTalon(RobotMap.INTAKE_MOTOR_RIGHT_CANID);
+
+		canTalonElevator = new CANTalon(RobotMap.ELEVATOR_MOTOR_CANID);
+		
+		
+		encoderLeft = new  Encoder(RobotMap.DRIVE_ENCODER_LEFT, RobotMap.DRIVE_ENCODER_LEFT2);
+		encoderRight = new Encoder(RobotMap.DRIVE_ENCODER_RIGHT, RobotMap.DRIVE_ENCODER_RIGHT2);
+
+		camServoHor = new Servo(RobotMap.CAMERA_SERVO_HORIZONTAL_PWM);
+		camServoVert = new Servo(RobotMap.CAMERA_SERVO_VERTICAL_PWM);
+		
+		//camera = new AxisCamera();
+		
+		compressor1 = new Compressor(RobotMap.PCM_CANID);
+		
+		leftHugPiston = new DoubleSolenoid(RobotMap.PCM_CANID, RobotMap.DOUBLE_SOLENOID_LEFT_HUG_PCMID_EXPANSION,RobotMap.DOUBLE_SOLENOID_LEFT_HUG_PCMID_RETRACTION);
+		rightHugPiston = new DoubleSolenoid(RobotMap.PCM_CANID, RobotMap.DOUBLE_SOLENOID_RIGHT_HUG_PCMID_EXPANSION, RobotMap.DOUBLE_SOLENOID_RIGHT_HUG_PCMID_RETRACTION);
+		intakePiston = new DoubleSolenoid(RobotMap.PCM_CANID, RobotMap.DOUBLE_SOLENOID_INTAKE_PCMID_EXPANSION, RobotMap.DOUBLE_SOLENOID_INTAKE_PCMID_RETRACTION);
+		
+		robotDrive = new RobotDrive(canTalonFrontLeft, canTalonRearLeft, canTalonFrontRight, canTalonRearRight); 
+    }
+
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+
+    }
+    
+    public void teleopInit() {
+    	
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+    	if (joystickRight.getRawButton(RobotMap.TANK_DRIVE_BUTTON)){
+    		isTankDrive = true;
+    	}
+    	if(joystickRight.getRawButton(RobotMap.ARCADE_DRIVE_BUTTON)){
+    		isTankDrive = false;
+    	}
+    	if (isTankDrive){
+    	robotDrive.tankDrive(joystickLeft, joystickRight);
+    	}
+    	if (isTankDrive == false){
+    	robotDrive.arcadeDrive(joystickRight); //ARCADE DRIVE IS THE RIGHT STICK!
+    	}
+    }
+
+    
+    /**
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+    	if (joystickRight.getRawButton(1)) {
+			canTalonFrontLeft.set(joystickRight.getY() * -1);
+
+		} else {
+			canTalonFrontLeft.set(0);
+		}
+		if (joystickRight.getRawButton(2)) {
+			canTalonFrontRight.set(joystickRight.getY() * -1);
+
+		} else {
+			canTalonFrontRight.set(0);
+		}
+//		if (joystickRight.getRawButton(3)) {
+//			canTalonRearLeft.set(joystickRight.getY() * -1);
+//
+//		} else {
+//			canTalonRearLeft.set(0);
+//		}
+//		if (joystickRight.getRawButton(4)) {
+//			canTalonRearRight.set(joystickRight.getY() * -1);
+//
+//		} else {
+//			canTalonRearRight.set(0);
+//		}
+//
+//		if (joystickRight.getRawButton(5)) {
+//			canTalonIntakeLeft.set(joystickRight.getY() * -1);
+//
+//		} else {
+//			canTalonIntakeLeft.set(0);
+//		}
+//		if (joystickRight.getRawButton(6)) {
+//			canTalonIntakeRight.set(joystickRight.getY() * -1);
+//
+//		} else {
+//			canTalonIntakeRight.set(0);
+//		}
+//
+//		if (joystickRight.getRawButton(7)) {
+//			canTalonElevator.set(joystickRight.getY() * -1);
+//
+//		} else {
+//			canTalonElevator.set(0);
+//		}
+//		if (joystickRight.getRawButton(8)) {
+//			canTalonElevator2.set(joystickRight.getY() * -1);
+//
+//		} else {
+//			canTalonElevator2.set(0);
+//		}
+		if (joystickOp.getRawButton(7)){
+			leftHugPiston.set(DoubleSolenoid.Value.kForward);
+		}
+		else if (joystickOp.getRawButton(8)){
+			leftHugPiston.set(DoubleSolenoid.Value.kReverse);
+		}
+		if (joystickOp.getRawButton(9)){
+			rightHugPiston.set(DoubleSolenoid.Value.kForward);
+		}
+		else if (joystickOp.getRawButton(10)){
+			rightHugPiston.set(DoubleSolenoid.Value.kReverse);
+		}
+		if (joystickOp.getRawButton(11)){
+			intakePiston.set(DoubleSolenoid.Value.kForward);
+		}
+		else if (joystickOp.getRawButton(12)){
+			intakePiston.set(DoubleSolenoid.Value.kReverse);
+		}
+    }
+    
+}
---
+++ src/org/usfirst/frc/team668/robot/RobotMap.java
@@ -0,0 +1,57 @@
+package org.usfirst.frc.team668.robot;
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+    // For example to map the left and right motors, you could define the
+    // following variables to use with your drivetrain subsystem.
+    // public static int leftMotor = 1;
+    // public static int rightMotor = 2;
+    
+    // If you are using multiple modules, make sure to define both the port
+    // number and the module. For example you with a rangefinder:
+    // public static int rangefinderPort = 1;
+    // public static int rangefinderModule = 1;
+	
+	public static final int JOYSTICK_LEFT_PORT = 0;
+	public static final int JOYSTICK_RIGHT_PORT = 1;
+	public static final int JOYSTICK_OP_PORT = 2;
+	
+	public static final int TANK_DRIVE_BUTTON = 11;
+	public static final int ARCADE_DRIVE_BUTTON = 10;
+	
+	public static final int DRIVE_MOTOR_FRONT_LEFT_CANID = 0;
+	public static final int DRIVE_MOTOR_FRONT_RIGHT_CANID = 1;
+	public static final int DRIVE_MOTOR_REAR_LEFT_CANID = 2;
+	public static final int DRIVE_MOTOR_REAR_RIGHT_CANID = 3;
+	
+	public static final int INTAKE_MOTOR_LEFT_CANID = 4;
+	public static final int INTAKE_MOTOR_RIGHT_CANID = 5;
+	public static final int ELEVATOR_MOTOR_CANID = 6;
+
+	
+	public static final int DRIVE_ENCODER_LEFT = 0;
+	public static final int DRIVE_ENCODER_LEFT2 = 1;
+	public static final int DRIVE_ENCODER_RIGHT = 2;
+	public static final int DRIVE_ENCODER_RIGHT2 = 3;
+	
+	public static final int CAMERA_SERVO_VERTICAL_PWM = 0;
+	public static final int CAMERA_SERVO_HORIZONTAL_PWM = 1;
+	
+	public static final int DOUBLE_SOLENOID_LEFT_HUG_PCMID_RETRACTION = 0;
+	public static final int DOUBLE_SOLENOID_LEFT_HUG_PCMID_EXPANSION = 1;
+	public static final int DOUBLE_SOLENOID_RIGHT_HUG_PCMID_RETRACTION = 2;
+	public static final int DOUBLE_SOLENOID_RIGHT_HUG_PCMID_EXPANSION = 3;
+	public static final int DOUBLE_SOLENOID_INTAKE_PCMID_RETRACTION = 4;
+	public static final int DOUBLE_SOLENOID_INTAKE_PCMID_EXPANSION = 5;
+	
+	public static final int PCM_CANID = 10;
+	public static final int PDP_CANID = 20;
+	
+	
+	
+	
+}
