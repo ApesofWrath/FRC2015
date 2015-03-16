b8a316d36158b853107a589ebb8a87ad6134343b
gautam-prab <gautam@venkusha.net>
2015-03-15 22:11:47 -0700

    Added comments and sensibility to spinAway

--- src/org/usfirst/frc/team668/robot/Intake.java
+++ src/org/usfirst/frc/team668/robot/Intake.java
@@ -26,10 +26,16 @@ public class Intake {
 		Robot.canTalonIntakeRight.set(0);
 	}
 	
+	/**
+	 * Spins the intake motors in the same direction in order to push away bins during the spin away tote autonomous.
+	 * 
+	 * @param speed
+	 * 		the speed to spin from -1.0 to 1.0; positive is clockwise, negative is counterclockwise
+	 */
 	public static void spinAway(double speed) {
 		//this is for the spin away tote autonomous as it requires that the two intake motors turn in the same direction
-		Robot.canTalonIntakeLeft.set(-speed);
-		Robot.canTalonIntakeRight.set(-speed);
+		Robot.canTalonIntakeLeft.set(speed);
+		Robot.canTalonIntakeRight.set(speed);
 		
 	}
 }
